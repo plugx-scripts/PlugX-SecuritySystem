c29e56cbfa107268b54ca994dd77b2cdb619d72c
PlugX Scripts <plugxsoftware26@gmail.com>
2025-12-16 22:30:05 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PlugX Scripts'
+description 'Security System'
+version '1.0.0'
+
+escrow_ignore {
+  'config.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_doorlock'
+}
+dependency '/assetpacks'
